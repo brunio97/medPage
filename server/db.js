5a4baf5cbff792c16446fa74984207b5e896625d
brunio97 <brusalinasv1997@gmail.com>
2024-02-22 11:37:35 -0500

    production

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://localhost:27017/project";
+const mongoURI =  "mongodb://root:MjgzMi1icnVzYWxp@127.0.0.1:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
