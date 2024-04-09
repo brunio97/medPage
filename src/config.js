3cb72ee8ba6da3cc9b66cbf071c1f2bfc9f7cf40
brunio97 <brusalinasv1997@gmail.com>
2024-04-09 12:45:39 -0400

    try

--- src/config.js
+++ src/config.js
@@ -3,13 +3,13 @@
 //     "API_URL :",
 //     API_URL
 // );  
-export const API_URL = window.location.origin === "localhost" ? "http://localhost:8181" : "http://localhost:8181";
-console.log(
-    "API_URL :",
-    API_URL
-);  
-// export const API_URL = window.location.origin 
+// export const API_URL = window.location.origin === "localhost" ? "http://localhost:8181" : "http://localhost:8181";
 // console.log(
 //     "API_URL :",
 //     API_URL
 // );  
+export const API_URL = window.location.origin 
+console.log(
+    "API_URL :",
+    API_URL
+);  
